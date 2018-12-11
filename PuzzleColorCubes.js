d7e98dcb8dd82d706b02560e523acc854706108d
Dithyboy2000 <43862544+Dithyboy2000@users.noreply.github.com>
2018-12-11 15:07:44 -0500

    Add files via upload

---
+++ PuzzleColorCubes.js
@@ -0,0 +1,23 @@
+function button1(){
+	document.getElementById("button1").style.backgroundColor = "blue"
+}
+function button2 () {
+	document.getElementById("button2").style.backgroundColor = "green"
+} 
+function button3 () {
+	document.getElementById("button3").style.backgroundColor = "orange"
+}
+function button4 () {
+	if (document.getElementById("button1").style.backgroundColor = "blue") {
+	document.getElementById("button4").style.backgroundColor = "yellow"}
+	else { 
+	(document.getElementById("button4").style.backgroundColor = "red");
+	(document.getElementById("button2").style.backgroundColor = "red");
+	(document.getElementById("button3").style.backgroundColor = "red");
+	(document.getElementById("button5").style.backgroundColor = "red");
+	}
+} 
+function button5 () {
+	
+	document.getElementById("button5").style.backgroundColor = "pink"
+} 
